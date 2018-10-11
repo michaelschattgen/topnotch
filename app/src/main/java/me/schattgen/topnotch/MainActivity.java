a131d446038696b410fecdfb8192523e828e688d
Michael Schättgen <michael@schattgen.me>
2018-10-11 22:32:59 +0200

    Started working on draw overlay permissions

--- app/src/main/java/me/schattgen/topnotch/MainActivity.java
+++ app/src/main/java/me/schattgen/topnotch/MainActivity.java
@@ -1,13 +1,59 @@
 package me.schattgen.topnotch;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!hasPermission())
+        {
+            getDrawOverlayPermission();
+        }
+    }
+
+    public boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                return false;
+            }
+
+            return true;
+        }
+
+        return true;
+    }
+
+    public void getDrawOverlayPermission() {
+
+        if(!hasPermission())
+        {
+            Intent permissionIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+            startActivityForResult(permissionIntent, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+
+        if (requestCode == REQUEST_CODE) {
+            if(hasPermission()){
+                // Success
+            }
+            else {
+                // No permission
+            }
+        }
     }
 }
