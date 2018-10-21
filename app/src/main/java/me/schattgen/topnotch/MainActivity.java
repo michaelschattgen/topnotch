1132693564b67585acd7fd5565826ba5a92ea549
Michael Schättgen <michael@schattgen.me>
2018-10-22 00:09:05 +0200

    Migrated to AndroidX :clap:

--- app/src/main/java/me/schattgen/topnotch/MainActivity.java
+++ app/src/main/java/me/schattgen/topnotch/MainActivity.java
@@ -4,11 +4,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
